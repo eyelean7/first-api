4b67212dff470c5316e30d18db3374cdd6466f6c
Max Reichman <reichmanmax@gmail.com>
2017-07-17 12:01:00 -0700

    Add more RESTful routing.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,26 @@
 class MessagesController < ApplicationController
   def index
     @group_id = params[:group_id]
-    @messages = Message.where(group_id: @group_id)
+    date = params[:date]
+    if params[:date]
+      @messages = Message.where(created_at: date)
+    elsif params[:group_id]
+      @messages = Message.where(group_id: @group_id)
+    else
+      @messages = Message.all
+    end
     json_response(@messages)
+  end
+
+  def create
+    @group_id = params[:group_id]
+    @message = Message.create!(message_params)
+    json_response(@message, :created)
   end
 
   private
 
   def message_params
-    params.permit(:content, :author, :id, :group_id)
+    params.permit(:content, :author, :id, :group_id, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   resources :groups do
     resources :messages
   end
